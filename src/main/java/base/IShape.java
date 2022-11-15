15a95f63c701ac600bb2f9c3f7cdc3f229526e90
ProwlingLynx <tamezjavier@gmail.com>
2022-11-14 21:39:54 -0600

    adding init interface IShape adding BaseShape class adding BaseShape fields, getter, setter, constructor.

---
+++ src/main/java/base/IShape.java
@@ -0,0 +1,6 @@
+package base;
+
+public interface IShape {
+
+    int calculateArea();
+}
---
+++ src/main/java/base/ShapeBase.java
@@ -0,0 +1,41 @@
+package base;
+
+public class ShapeBase implements IShape {
+    protected String name;
+    protected int width, height;
+
+    public ShapeBase(String name, int width, int height) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int calculateArea() {
+        return 0;
+    }
+}
